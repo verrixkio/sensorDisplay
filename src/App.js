89b783ab4c4ea9e8a347fb8e696edcd51b966abd
Thomas <thomas@Thomass-iMac.local>
2020-03-28 12:32:04 -0600

    added a function to localize date/time

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <div>
           <SensorDisplay/>
-        </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/SensorDisplay.jsx
+++ src/components/SensorDisplay.jsx
@@ -3,16 +3,25 @@ import React from 'react';
 var sensorData = require('../sensors.json');
 
 class SensorDisplay extends React.Component {
-    
+  
+  
+  dateLocalizer(date) {
+    let dateObj = new Date (date)
+    let localTime = dateObj.toLocaleTimeString();
+    let localDay = dateObj.toLocaleDateString();
+    return <h1>{localDay + " " + localTime}</h1>
+  }
+
   render() {
     return (
       <div>
+
         {sensorData.map(data => 
         <div>
           <div>{data.id}</div>
           <div>{data.name}</div>
           <div>{data.type}</div>
-          <div>{data.createdAt}</div>
+          <div>{this.dateLocalizer(data.createdAt)}</div>
           <div>{data.units}</div>
         </div>
         )}
