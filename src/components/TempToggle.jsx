70ddb98b032e759f22d29241c9e0028490f4e943
Thomas <thomas@Thomass-iMac.local>
2020-03-28 15:28:42 -0600

    Found a better location for the Round function being used

--- src/components/TempToggle.jsx
+++ src/components/TempToggle.jsx
@@ -4,16 +4,14 @@ function TempToggle(props) {
 
   const [value, setValue] = useState(props.value);
   const [isToggled, setToggled] = useState(props.unit);
-  
+
   const toggleTrueFalse = () => {
     if (isToggled === "Celsius") {
-      let toFarhenheit = (value * (9/5) + 32);
-      setValue((Math.round((toFarhenheit + Number.EPSILON) * 100) / 100));
+      setValue(value * (9/5) + 32);
       setToggled("Farhenheit");
 
     } else if (isToggled === "Farhenheit") {
-      let toCelsius = ((5/9) * (value-32));
-      setValue((Math.round((toCelsius + Number.EPSILON) * 100) / 100));
+      setValue((5/9) * (value-32));
       setToggled("Celsius");
     }
   }
@@ -21,7 +19,7 @@ function TempToggle(props) {
   return (
     <div className="App">
       <div className="container">
-        <h3>{value}</h3>
+        <h3>{(Math.round((value + Number.EPSILON) * 100) / 100)}</h3>
         <button onClick={toggleTrueFalse}>
           <h3>{isToggled}</h3>
         </button>
