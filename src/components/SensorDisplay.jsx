b30a34425025ba88f5ba66c4da6f7a50fe71f0a6
Thomas <thomas@Thomass-iMac.local>
2020-03-28 13:06:48 -0600

    Added function that maps the sensor to its specific readings

--- src/components/SensorDisplay.jsx
+++ src/components/SensorDisplay.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 var sensorData = require('../sensors.json');
+var readingData = require('../readings.json');
 
 class SensorDisplay extends React.Component {
   
@@ -9,21 +10,50 @@ class SensorDisplay extends React.Component {
     let dateObj = new Date (date)
     let localTime = dateObj.toLocaleTimeString();
     let localDay = dateObj.toLocaleDateString();
-    return <h1>{localDay + " " + localTime}</h1>
+    return <h3>{localDay + " " + localTime}</h3>
   }
+
+  // We want a function that takes in a sensor.
+
+  sensorMapper(sensor) {
+
+  //Now render out the readings with each specific sensor
+  let currentSensor = sensor.id
+
+  //Get the values from readings that match currentSensor
+  const readingValues = readingData.map(data => {
+    console.log(data.sensorId, ' reading data', currentSensor)
+    if (data.sensorId === currentSensor) {
+      return (
+        <div>
+          <p>{data.sensorId}</p>
+          <p>{data.time}</p>
+          <p>{data.value}</p>
+        </div>
+      )
+    }
+  })
+    return (
+    <div>
+      <h2>Sensor</h2>
+      <div>{sensor.id}</div>
+      <div>{sensor.name}</div>
+      <div>{sensor.type}</div>
+      <div>{this.dateLocalizer(sensor.createdAt)}</div>
+      <div>{sensor.units}</div>
+      <h2>Reading Data</h2>
+      <div>{readingValues}</div>
+    </div>
+    )
+  }
+  
 
   render() {
     return (
       <div>
-
-        {sensorData.map(data => 
-        <div>
-          <div>{data.id}</div>
-          <div>{data.name}</div>
-          <div>{data.type}</div>
-          <div>{this.dateLocalizer(data.createdAt)}</div>
-          <div>{data.units}</div>
-        </div>
+        <h1>Here are our sensors</h1>
+        {sensorData.map(sensor => 
+          this.sensorMapper(sensor)
         )}
       </div>
     );
