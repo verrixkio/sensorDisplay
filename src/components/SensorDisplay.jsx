51707d0169d9bd89eeb1d350a7e6a9fa2c9b3d4b
Thomas <thomas@Thomass-iMac.local>
2020-03-29 15:48:01 -0600

    Was running into reoccuring issues because my SensorMapper should've been a different component. Moved it and instituted the state requirements for hiding/displaying results.

--- src/components/SensorDisplay.jsx
+++ src/components/SensorDisplay.jsx
@@ -1,67 +1,15 @@
 import React from 'react';
-import TempToggle from './TempToggle';
+import SensorMapper from './SensorMapper';
 
 var sensorData = require('../sensors.json');
-var readingData = require('../readings.json');
-
 class SensorDisplay extends React.Component { 
 
-  //Function that will return the local time and day from a date string.
-  dateLocalizer(date) {
-    let dateObj = new Date (date)
-    let localTime = dateObj.toLocaleTimeString();
-    let localDay = dateObj.toLocaleDateString();
-    return localTime + " " + localDay
-  }
-  
-  //Function that takes a specific sensor and maps the reading data to that sensor.
-  sensorMapper(sensor) {
-
-  let currentSensor = sensor.id
-
-  //Build the table data by mapping through the readings.
-  const readingValues = readingData.map(data => {
-    if (data.sensorId === currentSensor) {
-      return (
-        <tr>
-          <td>{this.dateLocalizer(data.time)}</td>
-          <TempToggle unit={sensor.units} value={data.value}/>
-        </tr>
-      )
-    } else {
-        return null
-    }
-  })
-
-  //Return the built table and sensor data to our original map.
-  return (
-    <div>
-      <div className="sensor-data">
-        <h1>Sensor {sensor.id}</h1>
-        <h3>{sensor.name}</h3>
-        <h3>Type: {sensor.type}</h3>
-        <h3>Created: {this.dateLocalizer(sensor.createdAt)}</h3>
-      </div>
-      <div className="overflow-table">
-        <table>
-          <tbody>
-          <tr>
-            <th>Time of Reading</th>
-            <th>Value</th>
-          </tr>
-            {readingValues}
-          </tbody>
-        </table>
-      </div>
-    </div>
-    )
-  }
-
   render() {
+
     return (
       <div>
         {sensorData.map(sensor => 
-          this.sensorMapper(sensor)
+          <SensorMapper sensor={sensor}/>
         )}
       </div>
     );
